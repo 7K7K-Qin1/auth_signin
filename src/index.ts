eeb893312f56a9c9aef45bf8867935288780c59e
7k7k-Qin1 <tqinaf@connect.ust.hk>
2023-06-19 12:32:30 +0800

    FIX: INDEX

---
+++ src/index.ts
@@ -0,0 +1,7 @@
+export * from '../src/auth/auth.controller';
+export * from '../src/auth/auth.module';
+export * from '../src/auth/auth.service';
+export * from '../src/auth/auth.guard';
+export * from '../src/auth/dto/signIn.dto';
+export * from '../src/auth/dto/signUp.dto';
+export * from '../src/auth/dto/reset.dto';
